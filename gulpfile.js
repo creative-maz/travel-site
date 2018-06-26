a78080d4788d9f714069327dfc1eaed9ddf0d4c3
Mazhar Khan <kmazhar678@gmail.com>
2018-06-26 14:53:17 +0500

    added new css file and modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  watch = require('gulp-watch');
 
 gulp.task('default', function() {
   console.log('Hooray, Created a gulp Task');
+});
+
+gulp.task('html', function() {
+  console.log('Imagine so 9oaosd');
+});
+
+gulp.task('styles', function() {
+  console.log('Imagine so sass post css running here');
+});
+
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('.app/assets/styles/**/.css', function() {
+    gulp.start('html');
+  });
 });
